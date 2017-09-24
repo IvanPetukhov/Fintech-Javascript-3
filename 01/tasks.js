259672762bbf97dead6bc40eb1b3d4c7167e6a8e
Ivan Petukhov <Ivan.Petukhov.CMC@gmail.com>
2017-09-24 21:33:30 +0300

    Hometask1

--- 01/tasks.js
+++ 01/tasks.js
@@ -5,7 +5,65 @@
  * '1 и 6.45, -2, но 8, а затем 15, то есть 2.7 и -1028' => { min: -1028, max: 15 }
  */
 function getMinMax(string) {
-
+  let a = [];
+  let i = 0;
+  let str = string;
+  while (true){
+    const end1 = str.indexOf(' ');
+    const end2 = str.indexOf(',');
+    if((end1 === -1 || end1 === str.length - 1) && (end2 === -1 || end2 === str.length - 1)){
+      const k = parseFloat(str);
+      if(!isNaN(k)){
+        a[i] = k;
+      }
+      break;
+    } else if ((end1 === -1 || end1 === str.length - 1) && (end2 !== -1 && end2 !== str.length - 1)){
+      const x = parseFloat(str.slice(0, end2));
+      if(isNaN(x)){
+        str = str.slice(end2 + 1);
+      } else{
+        a[i] = x;
+        str = str.slice(end2 + 1);
+        i++;
+      }
+    } else if ((end2 === -1 || end2 === str.length - 1) && (end1 !== -1 && end1 !== str.length - 1)){
+      const y = parseFloat(str.slice(0, end1));
+      if(isNaN(y)){
+        str = str.slice(end1 + 1);
+      } else{
+        a[i] = y;
+        str = str.slice(end1 + 1);
+        i++;
+      }
+    } else{
+      const pos = end1 < end2 ? end1 : end2
+      const z = parseFloat(str.slice(0, pos));
+      if(isNaN(z)){
+        str = str.slice(pos + 1);
+      } else{
+        a[i] = z;
+        str = str.slice(pos + 1);
+        i++;
+      }
+    }
+    if (str === null)
+      break;
+  }
+  if (a.length !== 0){
+    let max = a[0];
+    let min = a[0];
+    for (let i = 0; i < a.length; i++){
+      if(a[i] > max){
+        max = a[i];
+      }
+      if(a[i] < min){
+        min = a[i];
+      }
+    }
+    let res = {"max" : max, "min" : min};
+    return res;
+  }
+  return {};
 }
 
 /* ============================================= */
@@ -16,7 +74,14 @@ function getMinMax(string) {
  * @return {number} число под номером х
  */
 function fibonacciSimple(x) {
-  return x;
+  if (x === 1){
+      return 1;
+  }
+  if (x === 2){
+      return 1;
+  }
+  let res = fibonacciSimple(x-2) + fibonacciSimple(x-1);
+  return res;
 }
 
 /* ============================================= */
@@ -27,8 +92,23 @@ function fibonacciSimple(x) {
  * @param {number} x номер числа
  * @return {number} число под номером х
  */
+let cache = {};
+
 function fibonacciWithCache(x) {
-  return x;
+    if (x in cache){
+        return cache[x];
+    }
+    if (x === 1){
+        cache[1] = 1;
+        return 1;
+    }
+    if (x === 2){
+        cache[2] =  1;
+        return 1;
+    }
+    let res = fibonacciWithCache(x-2) + fibonacciWithCache(x-1);
+    cache[x] = res;
+    return res;
 }
 
 /* ============================================= */
@@ -49,7 +129,32 @@ function fibonacciWithCache(x) {
  * @return {string}
  */
 function printNumbers(max, cols) {
-
+  let strNum = Math.ceil(max / cols);
+  let res = " ";
+  let a = [];
+  for(let i = 0; i <= max; i++){
+      a[Math.trunc(i / strNum) + (i % strNum) * cols] = i;
+  }
+  for(let i = 0; i < a.length; i++){
+      res += a[i];
+      if (i == a.length - 1){
+          break;
+      }
+      if((i + 1) % cols === 0){
+          if (Math.trunc(a[i + 1] / 10) === 0){
+              res += "\n ";
+          } else{
+              res += "\n";
+          }
+      } else{
+          if(Math.trunc(a[i + 1] / 10) === 0){
+              res += "  ";
+          } else{
+              res += " ";
+          }
+      }
+  }
+  return res;
 }
 
 /* ============================================= */
@@ -60,7 +165,22 @@ function printNumbers(max, cols) {
  * @return {string}
  */
 function rle(input) {
-
+  let i = 0;
+  let str = "";
+  while(i < input.length){
+      let count = 1;
+      while(input[i] === input[i + count]){
+          count++;
+      }
+      if(count !== 1){
+          str += input[i] + count;
+          i += count;
+      } else{
+          str += input[i];
+          i++;
+      }
+  }
+  return str;
 }
 
 module.exports = {
